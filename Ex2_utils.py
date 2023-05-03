0a7a2a875cda5137d5fac8f73575a8f3ec3517ee
Avichay3 <aviaimzina312@gmail.com>
2023-05-03 19:23:07 +0300

    first

---
+++ Ex2_utils.py
@@ -0,0 +1,10 @@
+import numpy as np
+from cv2 import cv2 as cv
+
+
+def myID() -> np.int:
+    """
+    Return my ID (not the friend's ID I copied from)
+    :return: int
+    """
+    return 211780267
