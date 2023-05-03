2f5e84bc3f81e248cfcdb42af8a7232ac3164f7a
Avichay3 <aviaimzina312@gmail.com>
2023-05-03 19:39:08 +0300

    add the convolution methods, needs to implement

--- Ex2_utils.py
+++ Ex2_utils.py
@@ -8,3 +8,21 @@ def myID() -> np.int:
     :return: int
     """
     return 211780267
+
+
+def conv1D(in_signal: np.ndarray, k_size: np.ndarray) -> np.ndarray:
+    """
+    Convolve a 1-D array with a given kernel
+    :param in_signal: 1-D array
+    :param k_size: 1-D array as a kernel
+    :return: The convolved array
+    """
+
+
+def conv2D(in_image: np.ndarray, kernel: np.ndarray) -> np.ndarray:
+    """
+    Convolve a 2-D array with a given kernel
+    :param in_image: 2D image
+    :param kernel: A kernel
+    :return: The convolved image
+    """
